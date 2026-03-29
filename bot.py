a8409359b7fe21c475ac816def8206ddb094ea15
pawan009-coder <nagtakanwarpkd@gmail.com>
2026-03-29 22:05:47 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -2345,16 +2345,15 @@ def handle_all(message):
 
     action = ai_data.get("action", "chat")
     target_name = ai_data.get("target_name", "").lower()
-    hindi_reply = ai_data.get("hindi_reply", "ठीक है बॉस।")
+        hindi_reply = ai_data.get("hindi_reply", "ठीक है बॉस।")
     try:
         amount = int(ai_data.get("amount", 0))
-    except:
+    except (ValueError, TypeError):
         amount = 0
     except:
         amount = 0
     except:
-        amount = 0
-
+      
     # --- 4. User Data & Powers ---
     u = get_user(message.from_user)
 
