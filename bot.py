3d6efaae75524cad8a4fd8831a92a88514c5710b
pawan009-coder <nagtakanwarpkd@gmail.com>
2026-03-30 22:31:28 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -2419,7 +2419,7 @@ def handle_all(message):
         
         try:
             payload = {
-            text = message.text.replace("/voice", "").strip(),
+            text = message.text.replace("/voice", "").strip()
             "model": "elvish"}
             print("Sending to RVC:", payload)
             response = requests.post(HF_API_URL, json=payload, timeout=60)
