ec8232d348ebec10ea51663ff1ea9bb349c64d4e
pawan009-coder <nagtakanwarpkd@gmail.com>
2026-04-15 07:46:17 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ from io import BytesIO
 # Baki purane imports rehne do (telebot, os, etc.)
 # ... आपके सभी पुराने imports ...
 from pyrogram import Client
-from pytgcalls import Client as PyTgCalls
+from pytgcalls import PyTgCalls
 from pytgcalls import StreamType
 from pytgcalls.types import AudioPiped
 import yt_dlp
