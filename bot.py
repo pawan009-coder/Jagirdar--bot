3573ba08673e470524637a8361a6debba5159eee
pawan009-coder <nagtakanwarpkd@gmail.com>
2026-04-18 06:23:57 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ from io import BytesIO
 # ... आपके सभी पुराने imports ...
 from pyrogram import Client
 from pytgcalls import PyTgCalls
-from pytgcalls.types.input_stream import AudioPiped
+from pytgcalls import PyTgCalls, AudioPiped
 import yt_dlp
 
 # --- असिस्टेंट क्लाइंट (Pyrogram) ---
